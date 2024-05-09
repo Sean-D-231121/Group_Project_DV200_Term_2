ed562223d90f7b5242192bceb5a511b4bf716903
Anemi Breytenbach <125360666+AnemiB@users.noreply.github.com>
2024-05-09 16:40:12 +0200

    Update App.js

--- plantcareapp/src/App.js
+++ plantcareapp/src/App.js
@@ -1,32 +1,31 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import PlantLibrary from './pages/PlantLibrary';
-import ProductsPage from './pages/ProductsPage';
-import Home from './pages/Home';
-import Appointments from './pages/Appointments';
-import Settings from './pages/Settings';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import PlantLibrary from './Pages/PlantLibrary';
+import ProductsPage from './Pages/ProductsPage';
+import Home from './Pages/Home';
+import Appointments from './Pages/Appointments';
+import Settings from './Pages/Settings';
+import NavBar from './Components/NavBar'; 
 import Splash from './pages/Splash';
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Splash />} />
-            <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/SignUp" element={<SignUp />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/PlantLibrary" element={<PlantLibrary />} />
-            <Route path="/ProductsPage" element={<ProductsPage />} />
-            <Route path="/Appointments" element={<Appointments />} />
-            <Route path="/Settings" element={<Settings />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/PlantLibrary" element={<PlantLibrary />} />
+          <Route path="/ProductsPage" element={<ProductsPage />} />
+          <Route path="/Appointments" element={<Appointments />} />
+          <Route path="/Settings" element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
