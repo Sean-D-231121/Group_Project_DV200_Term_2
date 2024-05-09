11ba3632dbc90ab9d32cae51aff3ab594f8f58bd
Anemi Breytenbach <125360666+AnemiB@users.noreply.github.com>
2024-05-09 16:49:40 +0200

    Update App.js

--- plantcareapp/src/App.js
+++ plantcareapp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import PlantLibrary from './Pages/PlantLibrary';
-import ProductsPage from './Pages/ProductsPage';
-import Home from './Pages/Home';
-import Appointments from './Pages/Appointments';
-import Settings from './Pages/Settings';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import PlantLibrary from './pages/PlantLibrary';
+import ProductsPage from './pages/ProductsPage';
+import Home from './pages/Home';
+import Appointments from './pages/Appointments';
+import Settings from './pages/Settings';
 import NavBar from './Components/NavBar'; 
 import Splash from './pages/Splash';
 import './App.css';
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar />
+        <NavBar /> 
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
