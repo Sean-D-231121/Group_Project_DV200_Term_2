d5e0a24bba2df50daeb7f7cc5f61596420dced98
Anemi Breytenbach <125360666+AnemiB@users.noreply.github.com>
2024-05-15 21:00:03 +0200

    Update Home.js

--- plantcareapp/src/pages/Home.js
+++ plantcareapp/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import '../App.css'
+import { Link } from 'react-router-dom';
+import '../App.css';
+import './Home.css';
 import UserPhoto from '../Userphoto.png'; 
 import MySideNav from '../Components/NavBar';
-
 
 const Home = () => {
     return (
@@ -10,7 +11,9 @@ const Home = () => {
             <MySideNav />
             <header>
                 <h1>Profile</h1>
+                <br />
                 <img src={UserPhoto} alt="User" className="user-photo" style={{ marginRight: '-40px', width: '12%' }} />
+                <br />
                 <h2>Mari</h2>
                 <p style={{ marginTop: '-15px' }}>@marinathompson123</p>
             </header>
@@ -26,8 +29,8 @@ const Home = () => {
                 marginTop: '3%',
                 boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}> 
                 <div style={{ textAlign: 'center' }}>
-                    <h3 style={{ fontWeight: 'bold' }}>Personal Information</h3>
-                    <p style={{ textAlign: 'center' }}>
+                    <h6 style={{ fontWeight: 'bold' }}>Personal Information</h6>
+                    <p style={{ textAlign: 'center', fontSize: '12px' }}>
                         Name: <br />
                         Marina Thompson <br /><br />
                         Username: <br />
@@ -37,7 +40,38 @@ const Home = () => {
                     </p>
                 </div>
             </div>
+            
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                <button className="update-button">
+                    <Link to="/settings" style={{ textDecoration: 'none', color: 'inherit' }}>Update Information</Link>
+                </button>
+            </div>
+            <br />
+            <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Current Appointments</h3>
+           
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                <button className="appointment-button">
+                    <Link to="/appointments" style={{ textDecoration: 'none', color: 'inherit' }}>Monstera - Monday 16/5</Link>
+                </button>
+                <button className="appointment-button">
+                    <Link to="/appointments" style={{ textDecoration: 'none', color: 'inherit' }}>Orchid - Friday 20/5</Link>
+                </button>
+                <button className="appointment-button">
+                    <Link to="/appointments" style={{ textDecoration: 'none', color: 'inherit' }}>Begonia - Wednesday 3/6</Link>
+                </button>
+                <button className="appointment-button">
+                    <Link to="/appointments" style={{ textDecoration: 'none', color: 'inherit' }}>Fern - Saturday 14/6</Link>
+                </button>
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                <p style={{ textAlign: 'center', textDecoration: 'underline', cursor: 'pointer' }}>
+                    <Link to="/appointments" style={{ color: 'inherit' }}>See More</Link>
+                </p>
+            </div>
+            <br /><br /><br /><br /><br /><br /><br /><br />
         </div>
+        
     );
 };
 
