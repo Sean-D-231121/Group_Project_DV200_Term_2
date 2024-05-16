7c20684e489ef5959a7c15dc773e7fa231368cb7
Ruan-Klopper <231280@virtualwindow.co.za>
2024-05-16 13:53:58 +0200

    PlantsLibrary wo Pop up
    Basic front end implementation, without popup cards

--- plantcareapp/src/pages/PlantLibrary.js
+++ plantcareapp/src/pages/PlantLibrary.js
@@ -1,8 +1,43 @@
-const PlantLibrary = () => {
-    return (
-    <div>
-        
+import "./PlantLibrary.css";
+import NavBar from "../Components/NavBar";
+
+const PlantLibraryObject = ({
+  PlantID,
+  PlantImage,
+  PlantName,
+  PlantDescription,
+}) => {
+  return (
+    <div className="plObjectMain">
+      <button className="plObjectCloseBTN">-</button>
+      <div className="plObjectImage"></div>
+      <h6>Plant name</h6>
+      <p>Plant description</p>
+      <button className="plObjectViewPlantHisBTN">Plant history</button>
     </div>
-    );
-}
-export default PlantLibrary
+  );
+};
+
+const PlantLibrary = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="plMain">
+        <h2 style={{ marginTop: "40px", marginBottom: "40px" }}>
+          Plants Library
+        </h2>
+        <div className="plLibrarySpace">
+          <button className="plLibraryAddPlamtBTN">Add plant</button>
+          <div className="plLibraryContainer">
+            <PlantLibraryObject />
+            <PlantLibraryObject />
+            <PlantLibraryObject />
+            <PlantLibraryObject />
+            <PlantLibraryObject />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PlantLibrary;
