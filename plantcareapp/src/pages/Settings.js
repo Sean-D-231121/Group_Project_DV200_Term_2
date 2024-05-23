e5293c0292ce8428cff61539c916f80d5ad1f232
Anemi Breytenbach <125360666+AnemiB@users.noreply.github.com>
2024-05-23 14:41:02 +0200

    Update Settings.js

--- plantcareapp/src/pages/Settings.js
+++ plantcareapp/src/pages/Settings.js
@@ -55,7 +55,7 @@ const Settings = () => {
                     </div>
                     <br />
                     <h5>Email:</h5>
-                    <p>@marinathompson123</p>
+                    <p>marinathompson123@gmail.com</p>
                     <div className="text-box-container">
                         <input
                             type="text"
